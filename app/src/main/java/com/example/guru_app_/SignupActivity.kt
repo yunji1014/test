052d5de3c78fa5d234928e9ac07792317c4fd095
1014y <7787yunji@gmail.com>
2024-07-18 16:41:37 +0900

    7.26 로그인화면

---
+++ app/src/main/java/com/example/guru_app_/SignupActivity.kt
@@ -0,0 +1,16 @@
+package com.example.guru_app_
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
