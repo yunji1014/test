a81c62172299a820fc0223eaac1efb993e14f6a0
1014y <7787yunji@gmail.com>
2024-07-23 20:53:32 +0900

    7.23 로그인 화면, 회원가입 화면, 메인화면, 검색화면

--- app/src/main/java/com/example/guru_app_/SignupActivity.kt
+++ app/src/main/java/com/example/guru_app_/SignupActivity.kt
@@ -1,20 +1,41 @@
 package com.example.guru_app_
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SignupActivity : AppCompatActivity() {
+    lateinit var back: TextView
+    lateinit var signupbutton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // id가 signin인 버튼을 찾습니다.
+        back = findViewById<TextView>(R.id.back)
+        signupbutton = findViewById<Button>(R.id.signupbutton)
+
+        // 버튼 클릭 리스너를 설정합니다.
+        back.setOnClickListener {
+            // 새로운 인텐트를 생성하여 SignupActivity로 이동합니다.
+            //var할시 오류.. 초기화문제 한번만 사용되기 때문에. 다시 돌아오는경우엔?
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
+
+        signupbutton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
