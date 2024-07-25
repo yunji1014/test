313739c51fd3de9bca3b4620ed944a5d077dd2e1
Hye-m-in <kus09127@naver.com>
2024-07-25 18:36:59 +0900

    중간저장(login, Shelf, AR)

---
+++ app/src/main/java/com/example/guru_app_/BookShelf.kt
@@ -0,0 +1,41 @@
+package com.example.guru_app_
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.tabs.TabLayout
+
+class BookShelf : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_book_shelf)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val tabLayout : TabLayout = findViewById(R.id.tab_layout)
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab){
+                when(tab.position){
+                    0 -> {
+
+                    }
+                    1 -> {
+
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+    }
+}
