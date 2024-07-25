0ff6f2a2f8cee205dd61415e7ba17d82d276d5ec
1014y <7787yunji@gmail.com>
2024-07-25 13:55:54 +0900

    7.25 로그인 화면, 회원가입 화면, 메인화면, 검색화면

--- app/src/main/java/com/example/guru_app_/HomeActivity.kt
+++ app/src/main/java/com/example/guru_app_/HomeActivity.kt
@@ -1,17 +1,35 @@
 package com.example.guru_app_
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SearchView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home)
 
+        val search = findViewById<SearchView>(R.id.search)
+        search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    val intent = Intent(this@HomeActivity, SearchActivity::class.java)
+                    intent.putExtra("query", query)
+                    startActivity(intent)
+                }
+                return false
+            }
 
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
     }
 }
--- app/src/main/java/com/example/guru_app_/SearchActivity.kt
+++ app/src/main/java/com/example/guru_app_/SearchActivity.kt
@@ -1,6 +1,7 @@
 package com.example.guru_app_
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,13 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val query = intent.getStringExtra("query")
+        if (query != null) {
+            // query를 사용하여 검색 결과를 표시하는 로직 추가
+        } else {
+            Toast.makeText(this, "검색어가 없습니다.", Toast.LENGTH_SHORT).show()
         }
+
     }
 }
