c3b7eb79f413fb73b12c241c52bb1ce25b9c04bb
Hye-m-in <kus09127@naver.com>
2024-07-25 20:09:43 +0900

    중간저장(login, Shelf, AR)

--- app/src/main/java/com/example/guru_app_/MainActivity.kt
+++ app/src/main/java/com/example/guru_app_/MainActivity.kt
@@ -1,15 +1,38 @@
 package com.example.guru_app_
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Paint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var pw_find: TextView
+    lateinit var signup: TextView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        pw_find = findViewById(R.id.pw_find)
+        signup = findViewById(R.id.signup)
+
+        pw_find.paintFlags = Paint.UNDERLINE_TEXT_FLAG
+        signup.setOnClickListener{
+            val intent = Intent(this, BookShelf::class.java)
+            startActivity(intent)
+        }
+
     }
 }
